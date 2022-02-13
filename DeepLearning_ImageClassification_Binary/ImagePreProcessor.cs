7ab86dc792ab296450b9053609c8c743779865ac
SpicyCatGames <61442059+SpicyCatGames@users.noreply.github.com>
2022-02-13 13:01:55 +0600

    Image Data Extraction done

--- DeepLearning_ImageClassification_Binary/ImagePreProcessor.cs
+++ DeepLearning_ImageClassification_Binary/ImagePreProcessor.cs
@@ -1,22 +1,52 @@
 ﻿using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
+using System;
+using System.IO;
+using System.Text;
+using CsvHelper;
 
 namespace ML_Project
 {
     public static class ImagePreProcessor
     {
-        public static void PreProcessImages(string dir, string fileExtension)
+        public static List<ExtractedData> PreProcessImages(string inputDir, string fileExtension)
         {
-            string[] files = Directory.GetFiles(dir, fileExtension);
+            List<ExtractedData> colorProfiles = new List<ExtractedData>();
+            string[] files = Directory.GetFiles(inputDir, fileExtension);
             foreach (var item in files)
             {
                 // extract all the pixels from this image
-                List<Rgba32> pixels = ImagePixels(dir, item);
+                List<Rgba32> pixels = ImagePixels(inputDir, item);
                 // green 81° to 140°
                 // yellow green 61° to 80°
                 // yellow 51° to 60°
+                ExtractedData colorProfile = new ExtractedData();
+                foreach (var pixel in pixels)
+                {
+                    var hslColor = ColorUtil.FromRGB(pixel);
+                    if (DiscardRGBPixel(pixel))
+                    {
+                        continue;
+                    }
+                    else if(hslColor.H.IsBetween(51f, 60f))
+                    {
+                        // yellow
+                        colorProfile.Yellow += 1;
+                    }
+                    else if (hslColor.H.IsBetween(60f, 80f))
+                    {
+                        colorProfile.YellowGreen += 1;
+                    }
+                    else if(hslColor.H.IsBetween(80f, 140f))
+                    {
+                        colorProfile.Green += 1;
+                    }
+                }
+                colorProfiles.Add(colorProfile);
+                Console.WriteLine($"{item.Substring(item.LastIndexOf('\\')+1)}, {colorProfile.Yellow},{colorProfile.YellowGreen},{colorProfile.Green}");
             }
+            return colorProfiles;
         }
 
         private static List<Rgba32> ImagePixels(string preprocessDir, string item)
@@ -33,18 +63,53 @@ namespace ML_Project
                         Rgba32 rgba32 = default;
                         image[x, y].ToRgba32(ref rgba32);
                         pixels.Add(rgba32);
-                        Console.WriteLine(rgba32.ToString());
+                        // Console.WriteLine(rgba32.ToString());
                     }
                 }
                 return pixels;
             }
         }
 
-        public struct ColorProfile
+        public struct ExtractedData
         {
-            public float Yellow;
-            public float Green;
-            public float Black;
+            public int Yellow;
+            public int Green;
+            public int YellowGreen;
+            public int Ripened;
+        }
+        private static bool ApproximatelyEqual(this float a, float b, float epsilon = float.Epsilon)
+        {
+            return Math.Abs(a - b) <= epsilon;
+        }
+        private static bool IsBetween(this float x, float StartInclusive, float endExclusive)
+        {
+            return x >= StartInclusive && x < endExclusive;
+        }
+        private static bool DiscardRGBPixel(Rgba32 rgba32)
+        {
+            byte maxAllColorThreshold = 240;
+            byte minAllColorThreshold = 25;
+            float minSaturation = 0.05f;
+            if (rgba32.A < 2)
+            {
+                return true;
+            }
+            if(rgba32.R > maxAllColorThreshold && rgba32.G > maxAllColorThreshold && rgba32.B > maxAllColorThreshold)
+            {
+                return true;
+            }
+            if (rgba32.R < minAllColorThreshold && rgba32.G < minAllColorThreshold && rgba32.B < minAllColorThreshold)
+            {
+                return true;
+            }
+
+            HSLAColor hSLAColor = ColorUtil.FromRGB(rgba32);
+            if (hSLAColor.S <= minSaturation)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
--- DeepLearning_ImageClassification_Binary/Program.cs
+++ DeepLearning_ImageClassification_Binary/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp.Processing;
+using static ML_Project.ImagePreProcessor;
 
 namespace ML_Project
 {
@@ -16,8 +17,11 @@ namespace ML_Project
             string preprocessDir = Path.Combine(WorkingDirectory, @"Preprocess");
             string preprocessInputDir = Path.Combine(preprocessDir, @"Input");
             string preprocessOutputDir = Path.Combine(preprocessDir, @"Output");
+            List<ExtractedData> colorProfiles = ImagePreProcessor.PreProcessImages(preprocessInputDir, "*.jpg");
 
-            ImagePreProcessor.PreProcessImages(preprocessInputDir, "*.jpg");
+            //Rgba32 rgba32 = new Rgba32(128, 157, 48);
+            //HSLAColor hSLAColor = ColorUtil.FromRGB(rgba32);
+            //Console.WriteLine(hSLAColor.H);
         }
 
 
